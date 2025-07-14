919ae5d4c85a9e011a5f8128edd586112e78840e
Vanish <63583678+spinesi95@users.noreply.github.com>
2025-07-14 11:17:40 +0200

    Update monitor_biglietti.py

--- monitor_biglietti.py
+++ monitor_biglietti.py
@@ -114,13 +114,15 @@ def controlla_biglietti():
         else:
             print("Nessuna variazione rilevata.")
             # Controlla se inviare la notifica "keep-alive"
-            timestamp_notifica = leggi_timestamp_notifica()
-            if (timestamp_attuale - timestamp_notifica) > (ORE_PER_NOTIFICA_ATTIVA * 3600):
+            timestamp_notifica = int(leggi_timestamp_notifica())
+            if (int(timestamp_attuale) - int(timestamp_notifica)) > int(ORE_PER_NOTIFICA_ATTIVA * 3600):
                 messaggio = f"✅ <b>Monitoraggio attivo</b>\n<i>Nessuna variazione da >{ORE_PER_NOTIFICA_ATTIVA} ore (controllo delle {orario_controllo})</i>\n\n<b>Stato attuale:</b>\n{dati_attuali}"
-                print(time.time(), " ",  timestamp_notifica, " ", (ORE_PER_NOTIFICA_ATTIVA * 3600))
-                
+                 
                 if invia_messaggio_telegram(messaggio, URL):
                     notifica_inviata = True
+                    
+            else
+                print("Invio messaggio non necessario")
 
         # Se una notifica è stata inviata con successo, aggiorna i file
         if notifica_inviata:
