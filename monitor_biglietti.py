7b6eb8977a711e8a2d1b9f26864f4c22d6da77b3
Vanish <63583678+spinesi95@users.noreply.github.com>
2025-07-14 11:44:38 +0200

    Update monitor_biglietti.py

--- monitor_biglietti.py
+++ monitor_biglietti.py
@@ -42,7 +42,7 @@ timestamp_attuale = int(time.time())
 def salva_timestamp_notifica():
     """Salva il timestamp attuale dopo aver inviato una notifica."""
     with open(FILE_TIMESTAMP_NOTIFICA, 'w') as f:
-        f.write(timestamp_attuale)
+        f.write(str(timestamp_attuale))
 
 def invia_messaggio_telegram(messaggio, url_bottone):
     """Invia un messaggio con un bottone inline."""
